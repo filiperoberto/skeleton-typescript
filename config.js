ecfeac99e8fc580bae8124fb904de63567877ddb
ferrugemjs <ferrugemjavascript@gmail.com>
2016-10-18 18:36:54 -0500

    import of css

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ System.config({
   },
 
   map: {
+    "css": "github:systemjs/plugin-css@0.1.23",
     "es6-shim": "npm:es6-shim@0.35.1",
     "event-emitter-lite": "npm:event-emitter-lite@1.0.5",
     "ferrugemjs": "npm:ferrugemjs@0.9.3",
@@ -47,6 +48,10 @@ System.config({
     },
     "npm:es6-shim@0.35.1": {
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:ferrugemjs@0.9.3": {
+      "css": "github:systemjs/plugin-css@0.1.31",
+      "incremental-dom": "npm:incremental-dom@0.4.1"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
