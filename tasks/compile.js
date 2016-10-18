a4c3ea80aebecc4491eee91d1a80d805a87185a5
ferrugemjs <ferrugemjavascript@gmail.com>
2016-10-18 18:35:15 -0500

    adjusts in task compile to compile ondemand

--- tasks/compile.js
+++ tasks/compile.js
@@ -4,14 +4,36 @@ var ts = require('gulp-typescript');
 
 var tsConfig = require("../tsconfig.json").compilerOptions;
 
+function compile_ts(path){
+    return gulp.src([
+            path
+            ,"./jspm_packages/npm/event-emitter-lite@*/*.d.ts"
+        ])
+        .pipe(ts(tsConfig))
+        .pipe(uglify())
+        .pipe(gulp.dest(tsConfig.outDir));
+};
+
 gulp.task('compile',function(){
-    //console.log(""+destPackagePathAppView);
+    return  compile_ts("./src/**/*.ts");
+});
+
+gulp.task('compile',function(){
     return gulp.src([
 			"./src/**/*.ts"
-            ,"./jspm_packages/npm/event-emitter-lite@1.0.5/*.d.ts"
+            ,"./jspm_packages/npm/event-emitter-lite@*/*.d.ts"
         ])
         .pipe(ts(tsConfig))
         .pipe(uglify())
         .pipe(gulp.dest(tsConfig.outDir));   
 });
+
+gulp.task('copy_assets',function(){
+    return gulp.src([
+        "./src/**/assets/**/*.*"
+    ])
+    .pipe(gulp.dest(tsConfig.outDir));
+});
+
+module.exports = compile_ts;
 
--- tasks/watch.js
+++ tasks/watch.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var proxy = require('http-proxy-middleware');
+var compileTs = require('./compile');
 
 gulp.task('static_serve', function() {
   connect.server({
@@ -14,15 +15,20 @@ gulp.task('reload',function(){
 	connect.reload();
 });
 
-
-
-gulp.task('watch',['compile','template'],function(){
+gulp.task('watch',['compile','template','copy_assets'],function(){
     gulp.run('static_serve');
     gulp.watch("./src/**/*.html", ['template']);
-    gulp.watch('./src/**/*.ts', ['compile']);
+    gulp.watch('./src/**/*.ts').on('change',function(file){
+        var search = "/";
+        var path = file.path.replace(/\\/g,"/");
+        var fileName = path.substring(path.lastIndexOf(search)+search.length,path.length);
+        compileTs("./src/**/"+fileName);
+    });
+    //gulp.watch('./src/**/*.ts', ['compile']);
+    gulp.watch('./src/**/assets/**/*.*', ['copy_assets']);
 
     gulp.watch('./dist/**/*.*').on('change',function(file){
-    	gulp.src(file.path).pipe(connect.reload());
+        gulp.src(file.path).pipe(connect.reload());
     });
 
 });
